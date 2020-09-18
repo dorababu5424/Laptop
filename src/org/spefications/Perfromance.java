9c94159ec648b338a5315985ff1e634aa3b645d3
dorababu5424 <dorababu5424@gamil.com>
2020-09-18 20:56:56 +0530

    add b

--- src/org/spefications/Perfromance.java
+++ src/org/spefications/Perfromance.java
@@ -1,5 +1,14 @@
 package org.spefications;
 
 public class Perfromance {
+	
+	private void laptop() {
+		// TODO Auto-generated method stub
+		System.out.println("hp laptop");
+	}
+	public static void main(String[] args) {
+		Perfromance n = new Perfromance();
+		n.laptop();
+	}
 
 }
