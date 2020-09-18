f3c0f24448ff0c026faa43afda776a286415020e
dorababu5424 <dorababu5424@gamil.com>
2020-09-18 21:18:50 +0530

    add

--- src/org/spefications/Perfromance.java
+++ src/org/spefications/Perfromance.java
@@ -6,10 +6,7 @@ public class Perfromance {
 		// TODO Auto-generated method stub
 		System.out.println("hp laptop");
 	}
-	public static void main(String[] args) {
-		Perfromance n = new Perfromance();
-		n.laptop();
-	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("dorababu laptop");
